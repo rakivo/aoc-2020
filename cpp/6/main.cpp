8bdede0efb0e4967aad0562b164424ac360258a8
MarkTyrkba <awfulmarco@gmail.com>
2024-04-16 01:48:39 +0300

    Day 6 in C++

---
+++ cpp/6/main.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+
+#include <set>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <unordered_map>
+
+#define usize size_t
+#define String std::string
+
+#define cout std::cout
+#define endl std::endl
+
+#define sc std::set<char>
+#define vs std::vector<std::string>
+#define ums std::unordered_map<char, usize>
+#define vvs std::vector<std::vector<std::string>>
+
+#define FILE_NAME "input.txt"
+
+usize part1(const vvs& content)
+{
+    usize result = 0;
+    for (const auto& group : content)
+    {
+        sc uniqs = {};
+        for (const auto& person: group)
+            for (const auto& c: person) uniqs.insert(c);
+    
+        result += uniqs.size();
+    }
+    return result;
+}
+
+usize part2(const vvs& content)
+{
+    usize result = 0;
+    for (const auto& group : content)
+    {
+        ums map = {};
+        for (const auto& person: group)
+            for (const auto& c: person) map[c]++;
+    
+        const usize N = group.size();
+        for (const auto& pair: map)
+            if (pair.second == N) result++;
+    }
+    return result;
+}
+
+// <===========================================================>
+
+void vvout(const vvs& vv)
+{
+    cout << "[";
+    for (usize i = 0; i < vv.size(); ++i)
+    {
+        cout << "[";
+        for (usize j = 0; j < vv[i].size(); ++j)
+        {
+            cout << vv[i][j];
+            if (j != vv[i].size() - 1) cout << ", ";
+        }
+        cout << "]";
+        if (i != vv.size() - 1) cout << ", ";
+    }
+    cout << "]" << endl;
+}
+
+const static vvs read_file(std::ifstream& f)
+{
+    vvs result = {};
+    vs temp = {};
+    String line;
+    while (getline(f, line))
+    {
+        if (line.empty())
+        {
+            result.emplace_back(temp);
+            temp.clear();
+        } else temp.emplace_back(line);
+    }
+    if (!temp.empty()) result.push_back(temp);
+
+    return result;
+}
+
+int main(void)
+{
+    std::ifstream f(FILE_NAME);
+    if (!f.is_open())
+    {
+        cout << "Failed to open " << FILE_NAME << endl;        
+        return 1;
+    }
+
+    vvs content = read_file(f);
+    
+    cout << "Part 1: " << part1(content) << endl; // 6735
+    cout << "Part 2: " << part2(content) << endl; // 3221
+
+    f.close();
+
+    return 0;
+}
